823fc8734a225ca8fdfb40b9568975f6ef90ba4f
Natanael <natanaelbandeira@gmail.com>
2022-07-19 11:36:32 -0600

    Add illustration, position it, and add first styling to accordion items

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,12 +1,13 @@
-const accordions = document.getElementsByClassName("accordion-item__button");
+const accordions = document.getElementsByClassName("item__button");
 
 for (let i = 0; i < accordions.length; i++) {
     let target = accordions[i].parentElement.nextElementSibling;
-    let svg = accordions[i].nextElementSibling;
+    let svg = accordions[i].firstChild.nextElementSibling;
+    console.log(svg);
 
     accordions[i].onclick = () => {
         console.log('clicked');
-        svg.classList.toggle("accordion-item__toggle--on");
+        svg.classList.toggle("item__toggle--on");
         let expanded = accordions[i].getAttribute('aria-expanded') === 'true' || false;
         accordions[i].setAttribute('aria-expanded', !expanded);
         target.hidden = expanded;
